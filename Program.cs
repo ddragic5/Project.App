7b7d3ea7c8eec0f0ab8a0136f2a20ff4c4e15c9d
ddragic5 <daniel.dragic123@gmail.com>
2021-10-18 15:23:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using static Project.Code.Program;
+
+namespace Project.App
+{
+    
+    public partial class Program
+    {
+
+        public static class Operations
+        {
+            public static void GetMenu()
+            {
+                Console.WriteLine("Choose between two operations (DISPLAY/ENLIST)");
+                string mystring = null;
+                mystring = Console.ReadLine();
+                string str = mystring.ToUpper();
+
+                switch (str)
+                {
+                    case "DISPLAY":
+                        ShowAllStudents();
+                        break;
+                    case "ENLIST":
+                        AddStudent();
+                        GetMenu();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation, try again!");
+                        GetMenu();
+                        break;
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                {
+                    Console.WriteLine("App is now ready!");
+                }
+                GetMenu();
+            }
+        }
+    }
+}
